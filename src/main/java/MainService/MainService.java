07a398c1e76811dae8c5396628de38e51260e19d
Ahmed Khaled <70283146+Ahmed-Khaled2@users.noreply.github.com>
2025-05-01 06:52:41 +0300

    Initial Commit
    -Adding Layout classes

---
+++ src/main/java/MainService/MainService.java
@@ -0,0 +1,8 @@
+package MainService;
+
+public class MainService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/MainService/ServiceInterface.java
@@ -0,0 +1,5 @@
+package MainService;
+
+public class ServiceInterface {
+    
+}
